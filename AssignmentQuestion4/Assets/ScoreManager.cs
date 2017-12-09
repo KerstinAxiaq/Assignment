a914716e786791b1c1e40a3a064b4025953d7efb
KerstinAxiaq <33456363+KerstinAxiaq@users.noreply.github.com>
2017-12-09 11:17:03 +0100

    Score
    Level 1
    -Text showing score in level
    -Script managing score
    -Script to add up total score

---
+++ AssignmentQuestion4/Assets/ScoreManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text textPlayer1;
+	public Text textPlayer2;
+	public int scorePlayer1;
+	public int scorePlayer2;
+	public int score;
+	public int max;
+	public string scene;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if(scorePlayer1==max || scorePlayer2==max){
+			SceneManager.LoadScene(scene);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D theCollision){
+		if(theCollision.gameObject.name == "Player2Score"){
+			Debug.Log ("right");
+			scorePlayer2 += score;
+			TotalScore.TotalPlayer2 += score;
+			textPlayer2.text = scorePlayer2+"";
+		}else if(theCollision.gameObject.name == "Player1Score"){
+			Debug.Log ("left");
+			scorePlayer1 += score;
+			TotalScore.TotalPlayer1 += score;
+			textPlayer1.text = scorePlayer1+"";
+		}
+	}
+
+		
+
+}
---
+++ AssignmentQuestion4/Assets/TotalScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalScore : MonoBehaviour {
+
+	private static int totalPlayer1,totalPlayer2;
+
+	public static int TotalPlayer1 {
+		get {
+			return totalPlayer1;
+		}
+		set {
+			totalPlayer1 = value;
+		}
+	}
+
+	public static int TotalPlayer2 {
+		get {
+			return totalPlayer2;
+		}
+		set {
+			totalPlayer2 = value;
+		}
+	}
+
+}
