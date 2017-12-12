38a6c6c1fccef18332b3d271045a08e06279c6b8
KerstinAxiaq <33456363+KerstinAxiaq@users.noreply.github.com>
2017-12-12 16:09:15 +0100

    Level 3
    Copied Level 2
    -Added script to make obstacles rotate

---
+++ AssignmentQuestion4/Assets/FindWinner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FindWinner : MonoBehaviour {
+
+	public Text textWinner;
+	public Text textScore;
+
+	// Use this for initialization
+	void Start () {
+		if(TotalScore.TotalPlayer2>TotalScore.TotalPlayer1){
+			Debug.Log ("p2");
+			textWinner.text = "Winner Player 2";
+			textScore.text = "Score: "+TotalScore.TotalPlayer2;
+		}else if(TotalScore.TotalPlayer2<TotalScore.TotalPlayer1){
+			Debug.Log ("p1");
+			textWinner.text = "Winner Player 1";
+			textScore.text = "Score: "+TotalScore.TotalPlayer1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
